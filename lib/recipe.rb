c6980d8aa0fa4bf99d8eefdccbad8f358af80cb8
Gaby Mendez <gmendez90@gmail.com>
2020-06-03 12:40:54 -0400

    Add #ingredients

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,8 +1,16 @@
 class Recipe
-  attr_reader :name, :ingredients_required
+  attr_reader :name, :ingredients_required, :ingredients
 
   def initialize(name)
     @name = name
     @ingredients_required = {}
+    @ingredients = []
   end
+
+  def add_ingredient(ingredient, qty)
+    @ingredients_required[ingredient] ||= 0
+    @ingredients_required[ingredient] += qty
+    @ingredients << ingredient unless @ingredients.include?(ingredient)
+  end
+
 end
--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -14,7 +14,22 @@ class RecipeTest < Minitest::Test
     assert_instance_of Recipe, @recipe1
   end
 
+  def test_it_has_attributes
+    assert_equal "Mac and Cheese", @recipe1.name
+  end
+
   def test_starts_with_no_ingredients_required
     assert_equal ({}), @recipe1.ingredients_required
   end
+
+  def test_can_add_ingredients
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+
+    assert_equal 6, @recipe1.ingredients_required[@ingredient1]
+    assert_equal 8, @recipe1.ingredients_required[@ingredient2]
+    assert_equal [@ingredient1, @ingredient2], @recipe1.ingredients
+  end
+
 end
