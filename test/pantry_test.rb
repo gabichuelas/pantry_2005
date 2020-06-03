a74a06d0fe9550d6bcf70de83436a59158b91703
Gaby Mendez <gmendez90@gmail.com>
2020-06-03 12:22:28 -0400

    Create Pantry tests and class

---
+++ test/pantry_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+
+class PantryTest < Minitest::Test
+  def setup
+    @pantry = Pantry.new
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+  end
+
+  def test_it_exists
+    assert_instance_of Pantry, @pantry
+  end
+
+  def test_it_starts_with_no_stock
+    assert_equal ({}), @pantry.stock
+  end
+
+  def test_it_can_check_ingredient_stock
+    assert_equal 0, @pantry.stock_check(@ingredient1)
+  end
+end
